0281ef1e9ef4fd3bf7843bfccb4f2aeb62f67d6a
Daniel Barbosa <daniel.barbosa@a4pm.com.br>
2024-07-29 18:04:06 -0300

    feat: Implementado segunda página das receitas, botao de voltar e tratamento da requisição

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,14 +1,18 @@
 <script lang="ts">
+import MostrarReceitas from './MostrarReceitas.vue';
 import SelecionarIngredientes from './SelecionarIngredientes.vue';
 import SuaLista from './SuaLista.vue';
+
+type Pagina = 'SelecionarIngredientes' | 'MostrarReceitas';
 
 export default {
   data() {
     return {
       ingredientes: [] as string[],
+      conteudo: 'SelecionarIngredientes' as Pagina
     };
   },
-  components: { SelecionarIngredientes, SuaLista },
+  components: { SelecionarIngredientes, SuaLista, MostrarReceitas },
   methods: {
     adicionarIngrediente(ingrediente: string) {
       this.ingredientes.push(ingrediente);
@@ -16,6 +20,9 @@ export default {
     removerIngrediente(ingrediente: string) {
       this.ingredientes = this.ingredientes.filter(iLista => ingrediente !== iLista);
     },
+    navegar(pagina: Pagina) {
+      this.conteudo = pagina;
+    }
   },
 };
 </script>
@@ -23,7 +30,14 @@ export default {
 <template>
   <main class="conteudo-principal">
     <SuaLista :ingredientes="ingredientes" />
-    <SelecionarIngredientes @adicionar-ingrediente="adicionarIngrediente" @remover-ingrediente="removerIngrediente"/>
+
+    <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'"
+    @adicionar-ingrediente="adicionarIngrediente" 
+    @remover-ingrediente="removerIngrediente"
+    @buscar-receitas="navegar('MostrarReceitas')"
+    />
+
+    <MostrarReceitas v-else-if="conteudo === 'MostrarReceitas'" @editar-receitas="navegar('SelecionarIngredientes')"/>
   </main>
 </template>
 
