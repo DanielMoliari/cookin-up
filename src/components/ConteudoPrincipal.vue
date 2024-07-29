dbe894ee02c941417efa735e560bb08a3fe1ec95
Daniel Barbosa <daniel.barbosa@a4pm.com.br>
2024-07-29 18:07:56 -0300

    feat: Fazer estados persistirem após re-renderização do componente

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -31,13 +31,16 @@ export default {
   <main class="conteudo-principal">
     <SuaLista :ingredientes="ingredientes" />
 
-    <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'"
-    @adicionar-ingrediente="adicionarIngrediente" 
-    @remover-ingrediente="removerIngrediente"
-    @buscar-receitas="navegar('MostrarReceitas')"
+    <KeepAlive include="SelecionarIngredientes" >
+      <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'"
+       @adicionar-ingrediente="adicionarIngrediente" 
+      @remover-ingrediente="removerIngrediente"
+       @buscar-receitas="navegar('MostrarReceitas')"
     />
+    
 
     <MostrarReceitas v-else-if="conteudo === 'MostrarReceitas'" @editar-receitas="navegar('SelecionarIngredientes')"/>
+  </KeepAlive>
   </main>
 </template>
 
