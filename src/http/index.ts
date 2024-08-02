20074893d1fba1bc83d30c163fe189a62de80d9a
Daniel Barbosa <daniel.barbosa@a4pm.com.br>
2024-08-02 16:53:10 -0300

    feat: Todas as imagens necessarias para as categorias e a rota alterada para a rota autoral.

--- src/http/index.ts
+++ src/http/index.ts
@@ -7,7 +7,7 @@ async function obterDadosURL<T>(url: string) {
 }
 
 export async function obterCategorias() {
-  return obterDadosURL<ICategoria[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json');
+  return obterDadosURL<ICategoria[]>('https://gist.githubusercontent.com/DanielMoliari/5dfd170d5fa395560c7b0baeeb2e937e/raw/65bb477ffeeb1f5a98738a34b0d1d60a6e3493d3/categorias-ingredientes.json');
 }
 
 export async function obterReceitas() {
